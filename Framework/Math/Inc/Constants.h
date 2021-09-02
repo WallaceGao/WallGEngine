8a9303b42cce19f135460e7df26f5faaa8b3416c
wallacegao <13730852881@163.com>
2021-09-02 03:11:59 +0000

    WallG - add HalfPi in Math::Constants Imported from Changeset 98

--- Framework/Math/Inc/Constants.h
+++ Framework/Math/Inc/Constants.h
@@ -2,7 +2,8 @@
 
 namespace WallG::Math::Constants
 {
-	constexpr float Pi = 3.14159f;
+	constexpr float Pi = 3.1415926535f;
+	constexpr float HalfPi = Pi * 0.5f;
 	constexpr float TwoPi = Pi * 2.0f;
 	constexpr float DegToRad = Pi/180.0f;
 	constexpr float RadToDeg = 180.0f/ Pi;
